b97e0a4becbe5163f13f195d2d2e1a589b33802e
Artem Vesnin <artemvesnin@gmail.com>
2022-12-07 15:32:09 +0000

    Added cross origins

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-import crud, models, schemas
 from database import SessionLocal, engine
+
+from fastapi.middleware.cors import CORSMiddleware
+
+import crud, models, schemas
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
 
 # Dependency
 def get_db():
